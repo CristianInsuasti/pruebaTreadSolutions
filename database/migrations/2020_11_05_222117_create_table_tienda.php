0f96840f5680a6ec38918b2431ed45926bb9a581
Cristian Insuasti <ceim243@gmail.com>
2020-11-05 17:28:55 -0500

    controlador, modelo y migraciones

---
+++ database/migrations/2020_11_05_222117_create_table_tienda.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableTienda extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tienda', function (Blueprint $table) {
+
+            $table->increments('id_tienda');
+            $table->integer('nombre', 50);
+            $table->date('fecha_apertura');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tienda');
+    }
+}
