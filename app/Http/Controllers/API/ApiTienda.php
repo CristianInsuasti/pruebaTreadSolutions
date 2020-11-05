eb6bdfedefe38c0749dfd2ffd7c5ba3732f443fd
Cristian Insuasti <ceim243@gmail.com>
2020-11-05 18:22:07 -0500

    Se corrigio el problema el cual consistia en que no se encontraba la clase, agregando una linea en el archivo RoutesServiceProvider.php, se agrego la primera ruta de prueba

--- app/Http/Controllers/API/ApiTienda.php
+++ app/Http/Controllers/API/ApiTienda.php
@@ -1,11 +1,26 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ApiTienda extends Controller
 {
-    //
+    // public function addTienda(Request $request)
+    // {
+    //     $tienda = new Tienda();
+    //     $tienda->nombre = $request->nombre;
+    //     $tienda->fecha_apertura = $request->fecha_apertura;
+
+    //     $tienda->save();
+
+        
+    //     if ($tienda) {
+    //         return response()->json(['menssage' => 'OK'], 201);
+    //     } else {
+    //         return response()->json(['menssage' => 'Faild'], 401);
+    //     }
+
+    // }
 }
